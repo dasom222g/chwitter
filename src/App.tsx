2178129575b3be720e12b30802b18950eb19cf82
dasom222g <dasom228@gmail.com>
2024-08-02 13:53:35 +0900

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import CreateAccount from "./pages/CreateAccount";
+
+// router 정의
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
